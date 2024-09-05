e5ebc9b729401ad50d5a8f78e8a905e9f0bf029c
super-gr0ver <e.dorodnov@gmail.com>
2024-09-05 14:42:37 +0300

    fix

--- src/pages/Structur.js
+++ src/pages/Structur.js
@@ -3,7 +3,14 @@ import React from "react";
 export function Structur() {
   return (
     <div className="main-page main-container structur">
-      <a href="https://gukolomna.ru/studentu/schedule/">Расписание</a>
+      {/* <a href="https://gukolomna.ru/studentu/schedule/">Расписание</a> */}
+      <iframe
+        src="https://mega.nz/folder/mdtRCCgZ#unl8cyEEjrA5wXKduqsrsA/folder/eI8WQJDS"
+        allowfullscreen="false"
+        height="500px"
+        width="500px"
+        style="border: none"
+      ></iframe>
     </div>
   );
 }
