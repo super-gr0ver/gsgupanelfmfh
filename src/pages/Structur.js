4ffc36954fa545a9156ef0eace0c2974310d73f4
super-gr0ver <e.dorodnov@gmail.com>
2024-09-05 17:08:48 +0300

    fix

--- src/pages/Structur.js
+++ src/pages/Structur.js
@@ -4,13 +4,7 @@ export function Structur() {
   return (
     <div className="main-page main-container structur">
       {/* <a href="https://gukolomna.ru/studentu/schedule/">Расписание</a> */}
-      <iframe
-        src="https://mega.nz/folder/mdtRCCgZ#unl8cyEEjrA5wXKduqsrsA/folder/eI8WQJDS"
-        allowfullscreen="false"
-        height="500px"
-        width="500px"
-        style="border: none"
-      ></iframe>
+      <object data="https://mega.nz/folder/mdtRCCgZ#unl8cyEEjrA5wXKduqsrsA/folder/eI8WQJDS"></object>
     </div>
   );
 }
