539f943f1f10e90b4d3a5f699f334b7c6c1d495d
super-gr0ver <e.dorodnov@gmail.com>
2024-09-06 09:31:13 +0300

    fix

--- src/pages/Structur.js
+++ src/pages/Structur.js
@@ -3,7 +3,7 @@ import React from "react";
 export function Structur() {
   return (
     <div className="main-page main-container structur">
-      {/* <a href="https://gukolomna.ru/studentu/schedule/">Расписание</a> */}
+      <a href="https://gukolomna.ru/studentu/schedule/">Расписание</a>
       <iframe
         src="../img/structur/20ИНФ-о-51.pdf"
         height="600px"
