0b8b70b50a3dcceff4049a0fe45f4d751af434b2
super-gr0ver <e.dorodnov@gmail.com>
2024-09-06 09:16:19 +0300

    fix

--- src/pages/Structur.js
+++ src/pages/Structur.js
@@ -4,7 +4,21 @@ export function Structur() {
   return (
     <div className="main-page main-container structur">
       {/* <a href="https://gukolomna.ru/studentu/schedule/">Расписание</a> */}
-      <object data="../img/structur/20ИНФ-о-51.pdf"></object>
+      <iframe
+        src="../img/structur/20ИНФ-о-51.pdf"
+        height="600px"
+        width="500px"
+        frameborder="0"
+        scrolling="yes"
+      ></iframe>
+      <iframe
+        src="../img/structur/20ФИ-о-51.pdf"
+        height="600px"
+        width="500px"
+        frameborder="0"
+        scrolling="yes"
+      ></iframe>
+
       <object data="../img/structur/20ФИ-о-51.pdf"></object>
       <object data="../img/structur/20ХИ-о-51.pdf"></object>
     </div>
