0ca64c0634168b8189c0313819634523e067dc45
super-gr0ver <e.dorodnov@gmail.com>
2024-09-06 16:46:30 +0300

    fix

--- src/pages/Structur.js
+++ src/pages/Structur.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import list1 from "../img/structur/1.jpg";
+import list1 from "../img/structur/1.pdf";
 import list2 from "../img/structur/2.pdf";
 const pdf1 = list1;
 const pdf2 = list2;
@@ -9,11 +9,8 @@ export function Structur() {
   return (
     <div className="main-page main-container structur">
       {/* <a href="https://gukolomna.ru/studentu/schedule/">Расписание</a> */}
-      <img src={pdf1} width="500" height="500" alt="vk-qr-code" />
-      <img src={pdf2} alt="vk-qr-code" />
-
-      <embed type="application/pdf" src={pdf2} width="500" height="700" />
-      <embed type="x-pdf" src={pdf2} width="500" height="700" />
+      <embed type="application/pdf" src={pdf1} width="500" height="900" />
+      <embed type="application/pdf" src={pdf2} width="500" height="900" />
     </div>
   );
 }
