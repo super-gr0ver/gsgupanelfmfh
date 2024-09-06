ecbab718bc9ab6cc3b447d76df49e788780eaab3
super-gr0ver <e.dorodnov@gmail.com>
2024-09-06 17:18:58 +0300

    fix

--- src/pages/Structur.js
+++ src/pages/Structur.js
@@ -2,6 +2,22 @@ import React from "react";
 
 import list1 from "../img/structur/20ИНФ-о-51.pdf";
 import list2 from "../img/structur/20ФИ-о-51.pdf";
+
+// Кол-во файлов в директории
+const fs = require("fs");
+const dir = "../img/structur/";
+let filesInDir = 0;
+
+fs.readdir(dir, (err, files) => {
+  filesInDir = files.length;
+});
+console.log(filesInDir);
+
+// Массив файлов
+const docs = [];
+for (let i = 0; i < filesInDir; i++) {
+  docs.push[`../img/structur/${i}.pdf`];
+}
 const pdf1 = list1;
 const pdf2 = list2;
 
@@ -9,8 +25,18 @@ export function Structur() {
   return (
     <div className="main-page main-container structur">
       {/* <a href="https://gukolomna.ru/studentu/schedule/">Расписание</a> */}
-      <embed type="application/pdf" src={pdf1} width="500" height="900" />
-      <embed type="application/pdf" src={pdf2} width="500" height="900" />
+      {/* <embed type="application/pdf" src={pdf2} width="500" height="900" /> */}
+      {docs.map((doc, i) => (
+        <div>
+          <h1>Группа {i}</h1>
+          <embed
+            type="application/pdf"
+            src={`${doc}`}
+            width="500"
+            height="1000"
+          />
+        </div>
+      ))}
     </div>
   );
 }
