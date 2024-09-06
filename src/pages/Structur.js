7f08a1790ed38102be613a0c1378b0ac02546473
super-gr0ver <e.dorodnov@gmail.com>
2024-09-06 17:27:48 +0300

    fix

--- src/pages/Structur.js
+++ src/pages/Structur.js
@@ -5,6 +5,7 @@ import list1 from "../img/structur/20ИНФ-о-51.pdf";
 import list2 from "../img/structur/20ФИ-о-51.pdf";
 
 // Кол-во файлов в директории
+const fs = require("fs");
 const dir = "../img/structur/";
 let filesInDir = 0;
 
