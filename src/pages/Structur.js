da95b0b0973b7268c44c9195c2fd37b2478c64e8
super-gr0ver <e.dorodnov@gmail.com>
2024-09-06 13:36:38 +0300

    fix

--- src/pages/Structur.js
+++ src/pages/Structur.js
@@ -1,11 +1,16 @@
 import React from "react";
 
+import list1 from "../img/structur/20ИНФ-о-51.pdf";
+import list2 from "../img/structur/20ФИ-о-51.pdf";
+const pdf1 = list1;
+const pdf2 = list2;
+
 export function Structur() {
   return (
     <div className="main-page main-container structur">
       {/* <a href="https://gukolomna.ru/studentu/schedule/">Расписание</a> */}
       <iframe
-        src="../img/structur/20ИНФ-о-51.pdf"
+        src="{pdf1}"
         height="600px"
         width="500px"
         frameborder="0"
@@ -13,7 +18,7 @@ export function Structur() {
       ></iframe>
 
       <iframe
-        src="../img/structur/20ФИ-о-51.pdf"
+        src="{pdf2}"
         height="600px"
         width="500px"
         frameborder="0"
